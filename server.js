7c50e1c18a937c52d8308ddcb462c7475c07a771
umairb1 <umairbashir95@hotmail.com>
2018-02-09 16:53:22 +0000

    Server is able to run on one terminal.

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var favicon = require('serve-favicon');
 var path = require('path');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
@@ -14,7 +15,7 @@ server.set('views', path.join(__dirname, 'views'));
 server.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
-//server.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+server.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 server.use(logger('dev'));
 server.use(bodyParser.json());
 server.use(bodyParser.urlencoded({ extended: false }));
