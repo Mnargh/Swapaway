b772bf209a80aa701a120341404b21463251ecdb
Tom Brandon <tom.j.v.brandon@gmail.com>
2018-02-09 17:00:52 +0000

    Added test for /users route succesfully responding to a request

---
+++ tests/users.js
@@ -0,0 +1,18 @@
+var expect = require('chai').expect;
+var request = require('request');
+var server = require('../server');
+var chai = require('chai'), chaiHttp = require('chai-http');
+
+chai.use(chaiHttp);
+
+describe('Users Routes', function(){
+
+  it("/users should return a status code of 200", function(done){
+    chai.request(server)
+      .get('/users')
+      .end(function (err, res) {
+        expect(res).to.have.status(200)
+        done()
+      });
+   });
+});
