383d68445c3747e79d42944589b24f382a561f42
umairb1 <umairbashir95@hotmail.com>
2018-02-13 14:45:30 +0000

    Added delete router for users with test passing.

--- routes/users.js
+++ routes/users.js
@@ -11,6 +11,9 @@ var User = require("../models/users");
 //     resave: false
 //   }));
 
+
+// Show all users
+
 router.get('/', function(req, res, next) {
   var users = User.find({});
   users.exec(function(err, users){
@@ -19,6 +22,7 @@ router.get('/', function(req, res, next) {
   });
 });
 
+// Create 
 router.post('/new', function(req, res, next) {
   
   var newUser = new User();
@@ -38,7 +42,7 @@ router.post('/new', function(req, res, next) {
     }
   });
 });
-
+// Read
 router.get('/:id', function(req, res, next) {
   User.findById(req.params.id, function(err, user) {
     if (err) res.send(err);
@@ -46,6 +50,7 @@ router.get('/:id', function(req, res, next) {
   });
 }); 
 
+// Delete
 router.delete('/:id', function(req, res, next) {
   User.remove({_id : req.params.id}, (err, result) => {
     res.json({ message: "user successfully deleted!", result });
