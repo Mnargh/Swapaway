e18fb25379c79d6233f01eb7d5589deb9ad39e0a
charles <charlesemery15@gmail.com>
2018-02-13 15:03:53 +0000

    crud implemented for an item

--- routes/items.js
+++ routes/items.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var mongoose = require('mongoose');
 var Item = require('../models/items')
 
+// show all items
 router.get('/', function(req, res, next){
   let items = Item.find({});
   items.exec( function(err, items){
@@ -11,6 +12,7 @@ router.get('/', function(req, res, next){
   });
 });
 
+// create a new item
 router.post('/new', function(req, res, next){
   var newItem = new Item();
   newItem.name = req.body.name
@@ -25,6 +27,36 @@ router.post('/new', function(req, res, next){
     else{
       return res.redirect('/');
     }
+  });
+});
+
+// Read
+router.get('/:id', function(req, res, next) {
+  Item.findById(req.params.id, function(err, item) {
+    if (err) res.send(err);
+    res.json(item);
+  });
+});
+
+// Update
+router.put('/:id', function(req, res, next) {
+  Item.update({_id : req.params.id}, {
+    name : req.body.name
+    picture : req.body.picture
+    description : req.body.description
+    owner : req.body.owner
+    },
+    function(err, item) {
+        if (err) res.send(err);
+        else res.redirect('/items/'+ req.params.id);
+    });
+  });
+});
+
+// Delete
+router.delete('/:id', function(req, res, next) {
+  Item.remove({_id : req.params.id}, (err, result) => {
+    res.json({ message: "Item successfully deleted!", result });
   });
 });
 
