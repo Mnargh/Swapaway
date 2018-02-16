a47e2df41b1cddcaba5386d6c69c4351b821fb5f
Tom Brandon <tom.j.v.brandon@gmail.com>
2018-02-16 09:49:17 +0000

    Added logo to homepage. Seperated out buttons

--- client/src/components/pages/homepage.js
+++ client/src/components/pages/homepage.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { Button } from "react-bootstrap";
-
+import logo from '.././navbar/swapaway-dark.png'
 
 
 class HomePage extends Component {
@@ -8,9 +8,10 @@ class HomePage extends Component {
     return (
       <div className="HomePage">
         <center>
-          <p>Logo here</p>
+          <img src={logo}/><p>
+          </p>
           <Button id="ViewItemsbutton" href="/items" bsSize="large">View Items</Button>
-          <Button id="Register" href="/signup" bsSize="large">Register</Button>
+          {'   '}<Button id="Register" href="/signup" bsSize="large">Register</Button>
         </center>
       </div>
     );
